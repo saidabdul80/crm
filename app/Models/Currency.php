01588540ba57f9be395ea7ce7d4a5292b1f6e3cb
saidabdul80 <saidabdulsalam5@gmail.com>
2024-05-21 22:13:06 +0100

    build

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace Crater\Models;
-use Scottlaurent\Accounting\Models\TransactionGroup;
+//use Scottlaurent\Accounting\Models\TransactionGroup;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Scottlaurent\Accounting\ModelTraits\AccountingJournal;
+//use Scottlaurent\Accounting\ModelTraits\AccountingJournal;
 
 class Currency extends Model
 {
-    use HasFactory, AccountingJournal;
+    use HasFactory;
     protected $with = ['journal'];
 
     protected $fillable = [
